a46fe9f3ce99e0e8b3a87a05b59ed7284616944c
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-03-16 22:33:18 +0900

    棒グラフを描画できるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,40 @@
 <template>
-
+  <div>
+    aaa
+    <canvas ref="mainGraph"></canvas>
+  </div>
 </template>
+<script lang="ts" setup>
+import Chart from "chart.js/auto"
+
+import { onMounted, ref } from "vue"
+
+const mainGraph = ref<HTMLCanvasElement>()
+
+const mainLabel = [1, 2, 3, 4, 5, 6]
+const mainData = [12, 19, 3, 5, 2, 3]
+
+onMounted(()=>{
+  console.log(mainGraph.value);
+  if(mainGraph.value != undefined){    
+    new Chart(mainGraph.value, {
+      type: 'line',
+      data: {
+        labels: mainLabel,
+        datasets: [{
+          label: 'たぶん何かのデータ',
+          data: mainData,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+})
+</script>
